bc8fa29c011c7b50ebdb11a23a835768a287f094
Arcbhatt <146052162+Arcbhatt@users.noreply.github.com>
2024-03-10 00:53:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,26 @@ def home():
 def chat():
     return render_template("st.html")
     
+@app.route('/process_form', methods=['POST'])
+
+def process_form():
+
+    file_path = "C:\\Users\\anbha\\OneDrive\\Documents\\Projects\\chatbot\\messages.txt"
+    msg = request.form['Message']
+    
+    out = ""
+
+    client.client_f("Arc",msg,1)
+  
+    print(msg)
+
+    with open(file_path,'a') as f:
+        f.write("|"+msg + "|\n")
+
+    with open(file_path, 'r') as f:
+        out = f.read()
+        
+    return render_template('index.html', msg=out)
 
     
 if __name__ == '__main__':
