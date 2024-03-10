72273837f962567b95fa4f2d5878038b3f45f526
squirreler <dbbemailme@gmail.com>
2024-03-10 00:43:03 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,11 @@ app = Flask(__name__, template_folder= 'templates')
 @app.route('/')
 
 def home():
-    return render_template("st.html")
     return render_template("structure.html") 
 
 @app.route('/chat')
 def chat():
-    
+    return render_template("st.html")
     
 
     
