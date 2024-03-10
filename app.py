8396a8654d1f8bdfb59bcb15869ef449f292495d
Wutappp <86125764+Wutappp@users.noreply.github.com>
2024-03-10 01:51:59 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import client
 
-app = Flask(__name__, template_folder= 'templates')
+app = Flask(__name__, template_folder='templates')
 
-#app.config['SEVER_NAME'] = 'domain.com'
+# app.config['SEVER_NAME'] = 'domain.com'
 
 @app.route('/')
-
 def home():
     return render_template("structure.html") 
 
-
-    
 @app.route('/process_form', methods=['POST'])
-
 def process_form():
-
     file_path = "messages.txt"
-    msg = request.form['Message']
-    
+    msg = request.form.get('Message', '')  # Using get() to avoid IndexError
     out = ""
 
-    client.client_f("Arc",msg,1)
-  
-    print(msg)
+    if msg:  # Checking if msg is not empty
+        client.client_f("Arc", msg, 1)
+        print(msg)
 
-    with open(file_path,'a') as f:
-        f.write("|"+msg + "|\n")
+        with open(file_path, 'a') as f:
+            f.write("|" + msg + "|\n")
 
     with open(file_path, 'r') as f:
         out = f.read()
         
     return render_template('index.html', msg=out)
 
-    
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
 
 print("hello")
-
-
-
-
-
-
-
-
-
-
-
-
