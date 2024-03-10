7af7f7bb1de68217fb879c43acc491de711d4da5
squirreler <dbbemailme@gmail.com>
2024-03-09 22:23:30 -0800

    Added app.py, starting to commit everyones code

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder= '/Users/kevinwu/Desktop/Hackaton/')
 
-@app.route("/")
+#app.config['SEVER_NAME'] = 'domain.com'
 
-def hello_world():
-  return "<p>Hello, World!<p>"
+@app.route('/')
+
+def home():
+    return render_template("structure.html")
+
+@app.route('/chat')
+def chat():
+    return render_template("chat.html")
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
+print("hello")
+
+
+
+
+
+
+
